dd812fbfed44cbe22d573e81702d55aea347807c
Viktor Rosendahl <ruxiaowei@gmail.com>
2023-04-25 14:55:10 +0200

    Changed the instruction comment for specifying chrome user. Implemented changes stated in "Timestamp verification implemented" in Clickup.

--- src/main/java/org/example/FBPosting.java
+++ src/main/java/org/example/FBPosting.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.LocalTime;
+
 public class FBPosting {
     public static void main(String[] args) throws InterruptedException {
 
@@ -17,8 +19,9 @@ public class FBPosting {
         options.addArguments("--disable-notifications");
         /* This option opens the Chrome window with specified user profile.
         It is useful for when you wish to already be logged in.
-        Replace the dir path with the path to your own user. */
-        options.addArguments("--user-data-dir=C:\\Users\\adamj\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 2");
+        Replace the dir path with the path to your own user, and make sure to be logged in to facebook on your
+        chrome user before executing the code. */
+         options.addArguments("--user-data-dir=C:\\Users\\ruxia\\AppData\\Local\\Google\\Chrome\\User Data\\Default");
 
         try {
             // Create an instance of ChromeDriver with the options
@@ -34,9 +37,10 @@ public class FBPosting {
             Thread.sleep(1000);
             Logback.navigationSuccess();
 
-            // Find & put down sample post text into text field
+            // Find & put down current time into text field
+            String time = String.valueOf(LocalTime.now());
             WebElement active = driver.switchTo().activeElement();
-            active.sendKeys("You merely adopted the dark. I was born in it, molded by it.");
+            active.sendKeys("Current time is " + time);
             Thread.sleep(1000);
 
             /* Could not locate the element for the post button, so this uses
@@ -46,11 +50,15 @@ public class FBPosting {
                 active = driver.switchTo().activeElement();
             }
             active.sendKeys(Keys.ENTER);
+            // Giving FB time to post after clicking
+            Thread.sleep(3000);
+
 
             // Find the post, loggs the outcome
             try {
-                // Obviously only works with one unique post...
-                WebElement post = driver.findElement(By.xpath("//*[contains(text(), 'adopted the dark')]"));
+                // Creating temporary String containing the xpath expression + variable time.
+                String xpathPath = "//*[contains(text(), '" + time + "')]";
+               driver.findElement(By.xpath(xpathPath));
                 Logback.postSuccess();
             } catch (Exception all) {
                 Logback.postFailure();
